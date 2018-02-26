b15fcfb939209ecc09fbd9c22efd7381802d5829
Collin Barlage <cjb356@drexel.edu>
2018-02-25 19:47:39 -0500

    Implemented creating a new game via axios
    Run npm install on the jawn

--- checkers-frontend/src/components/Home.js
+++ checkers-frontend/src/components/Home.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import '../index.css';
 
-const Home = () => (
-    <div>
-      <h1> Checkers: The Game </h1>
-      <p> To begin, press <button>New Game </button> <br />
-        or enter a game code you got from a friend: 
-      </p>
-    </div>
-)
+function newGame(){
+	axios.request({
+		method:'post',
+		url:'http://localhost:1337/api/games',
+	}).then(response=> {
+		this.props.history.push('/');
+	}).catch(err => console.log(err));
+}
+
+class Home extends Component{
+	render(){
+		return (
+		    <div>
+		      <h1> Checkers: The Game </h1>
+		      <p> To begin, press <button onClick={newGame}>New Game </button> <br />
+		        or enter a game code you got from a friend: 
+		      </p>
+		    </div>
+	    )
+	}
+}
+
+
 
 export default Home;
