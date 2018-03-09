6ef84767b868fa8a30ce87b1337ba8a219fa0b4a
James A Shackleford <shack@linux.com>
2018-03-08 22:25:57 -0500

    Squares now in 2D array, simplified initialization

--- checkers-frontend/src/components/Game.js
+++ checkers-frontend/src/components/Game.js
@@ -15,190 +15,121 @@ function swapPlayer(player) {
 }
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+	return (
+		<button className="square" onClick={props.onClick}>
+		{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: [],
-      player: PLAYER_W
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: [],
+			player: PLAYER_W
+		};
+	}
 	
-  componentDidMount() {
-	const W_START_POS = [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23];
-	const B_START_POS = [40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62];
-	  
-	let t_squares = Array(64).fill('_');
-	W_START_POS.forEach(index => t_squares[index] = 'W');
-	B_START_POS.forEach(index => t_squares[index] = 'B');
+	componentDidMount() {
+		this.initializeBoard()
+	}
 	
-	this.setState({squares: t_squares});
-  }
+	handleClick(r, c) {
+		//emit an event
+		sendMove(gameKey, "ooo here's some move data");
+		
+		const squares = this.state.squares.slice();
+		squares[r][c] = (this.state.player === PLAYER_W ? 'W' : 'B');
+		this.setState({
+			squares: squares,
+			//player: swapPlayer(this.state.player)
+		});
+	}
 	
-  handleClick(i) {
-    //emit an event
-    sendMove(gameKey, "ooo here's some move data");
-
-    const squares = this.state.squares.slice();
-    squares[i] = (this.state.player === PLAYER_W ? 'W' : 'B');
-    this.setState({
-      squares: squares,
-      player: swapPlayer(this.state.player)
-    });
-  }
-  
-  initializeBoard() {
+	initializeBoard() {
+		const W_START_POS = [[0,1], [0,3], [0,5], [0,7],
+										 		 [1,0], [1,2], [1,4], [1,6],
+										 		 [2,1], [2,3], [2,5], [2,7]];
+		const B_START_POS = [[5,0], [5,2], [5,4], [5,6],
+												 [6,1], [6,3], [6,5], [6,7],
+												 [7,0], [7,2], [7,4], [7,6]];
+		
+		let t_squares = [...Array(8).keys()].map(i => Array(8));
+		W_START_POS.forEach(indexes => t_squares[indexes[0]][indexes[1]] = 'W');
+		B_START_POS.forEach(indexes => t_squares[indexes[0]][indexes[1]] = 'B');
+		
+		this.setState({squares: t_squares});
+	}
 	
-  }
+	renderSquare(r, c) {
+		return (
+			<Square
+			key={r*8 + c}
+			value={this.state.squares[r][c]}
+			onClick={() => this.handleClick(r, c)}
+			/>
+		);
+	}
 	
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const status = 'Next player: ' + (this.state.player === PLAYER_W ? 'W' : 'B');
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-		  {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-		  {this.renderSquare(6)}
-          {this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-		  {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-		  {this.renderSquare(14)}
-          {this.renderSquare(15)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-		  {this.renderSquare(19)}
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-		  {this.renderSquare(22)}
-          {this.renderSquare(23)}
-        </div>
-		<div className="board-row">
-          {this.renderSquare(24)}
-          {this.renderSquare(25)}
-          {this.renderSquare(26)}
-		  {this.renderSquare(27)}
-          {this.renderSquare(28)}
-          {this.renderSquare(29)}
-		  {this.renderSquare(30)}
-          {this.renderSquare(31)}
-        </div>
-		<div className="board-row">
-          {this.renderSquare(32)}
-          {this.renderSquare(33)}
-          {this.renderSquare(34)}
-		  {this.renderSquare(35)}
-          {this.renderSquare(36)}
-          {this.renderSquare(37)}
-		  {this.renderSquare(38)}
-          {this.renderSquare(39)}
-        </div>
-		<div className="board-row">
-          {this.renderSquare(40)}
-          {this.renderSquare(41)}
-          {this.renderSquare(42)}
-		  {this.renderSquare(43)}
-          {this.renderSquare(44)}
-          {this.renderSquare(45)}
-		  {this.renderSquare(46)}
-          {this.renderSquare(47)}
-        </div>
-		<div className="board-row">
-          {this.renderSquare(48)}
-          {this.renderSquare(49)}
-          {this.renderSquare(50)}
-		  {this.renderSquare(51)}
-          {this.renderSquare(52)}
-          {this.renderSquare(53)}
-		  {this.renderSquare(54)}
-          {this.renderSquare(55)}
-        </div>
-		<div className="board-row">
-          {this.renderSquare(56)}
-          {this.renderSquare(57)}
-          {this.renderSquare(58)}
-		  {this.renderSquare(59)}
-          {this.renderSquare(60)}
-          {this.renderSquare(61)}
-		  {this.renderSquare(62)}
-          {this.renderSquare(63)}
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const status = 'Next player: ' + (this.state.player === PLAYER_W ? 'W' : 'B');
+		
+		let result = [];
+		result.push(<div className="status" key="status">{status}</div>);
+		
+		for (let r = 0; r < this.state.squares.length; r++) {
+			const row = this.state.squares[r];
+			let temp = [];
+			for (let c = 0; c < row.length; c++) {
+				temp.push(this.renderSquare(r, c))
+			}
+			result.push(<div className="board-row" key={r}>{temp}</div>)
+		}
+		
+		return(result);
+	}
 }
 
 //called when opponent attempts to make a move
 function receiveMove(moveData) { 
-  console.log(moveData);
-  //TODO: ...
+	console.log(moveData);
+	//TODO: ...
 }
 
-//called when a user has joined
 function userJoined(joinData) {
-  console.log("user", joinData, "has joined!");
-  //TODO: handle when second user joined (AKA start game)
+	console.log("user", joinData, "has joined!");
 }
 
 class Game extends React.Component {
-  //connect to socket
-  constructor(props) {
-    super(props);
-    //get querry params
-    var params = this.props.location.search;
-    gameKey = params.substring(5, params.indexOf('&'));
-    clientPlayer = params.substring(49);
-    //subscribe to socket
-    subscribeToGame(gameKey, clientPlayer, (err, moveData) => {
-      receiveMove(moveData); 
-    }, (err, joinData) => {
-      userJoined(joinData); 
-    });
-  }
-
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+	//connect to socket
+	constructor(props) {
+		super(props);
+		//get querry params
+		var params = this.props.location.search;
+		gameKey = params.substring(5, params.indexOf('&'));
+		clientPlayer = params.substring(49);
+		//subscribe to socket
+		subscribeToGame(gameKey, clientPlayer, (err, moveData) => {
+			receiveMove(moveData); 
+		}, (err, joinData) => {
+			userJoined(joinData); 
+		});
+	}
+	
+	render() {
+		return (
+			<div className="game">
+			<div className="game-board">
+			<Board />
+			</div>
+			<div className="game-info">
+			<div>{/* status */}</div>
+			<ol>{/* TODO */}</ol>
+			</div>
+			</div>
+		);
+	}
 }
 
 export default Game;
