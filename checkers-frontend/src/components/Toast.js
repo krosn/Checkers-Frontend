1524921d6cb7e6efc0366d3ca27185eb69d09e74
Collin Barlage <cjb356@drexel.edu>
2018-03-11 18:55:03 -0400

    Move toast to top left

--- checkers-frontend/src/components/Toast.js
+++ checkers-frontend/src/components/Toast.js
@@ -10,30 +10,7 @@ style({
   colorError: "#e74c3c",
   colorProgressDefault: "linear-gradient(to right, #9d98ff,  #d3d2f8)",
   mobile: "only screen and (max-width : 480px)",
-  zIndex: 9999,
-  TOP_LEFT: {
-    left: '1em'
-  },
-  TOP_CENTER: {
-    marginLeft: `-${320/2}px`,
-    left: '50%'
-  },
-  TOP_RIGHT: {
-    right: '1em'
-  },
-  BOTTOM_LEFT: {
-    bottom: '2em',
-    left: '1em'
-  },
-  BOTTOM_CENTER: {
-    bottom: '2em',
-    marginLeft: `-${320/2}px`,
-    left: '50%'
-  },
-  BOTTOM_RIGHT: {
-    bottom: '2em',
-    right: '1em'
-  }
+  zIndex: 9999
 });
 
 export { ToastContainer, toast, style, css }  ;
