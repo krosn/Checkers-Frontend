09575f1dcfa04554f8b35fd91250927486137a46
Collin Barlage <cjb356@drexel.edu>
2018-03-11 16:09:43 -0400

    Moved toast stlye

--- checkers-frontend/src/components/Game.js
+++ checkers-frontend/src/components/Game.js
@@ -5,41 +5,6 @@ import { subscribeToGame, subscribeToBoard, sendMove, endTurn } from './Socket';
 import Move, {movedTwoSpaces} from './Move';
 import Piece from './Piece';
 
-style({
-  width: "320px",
-  colorDefault: "#000",
-  colorInfo: "#3498db",
-  colorSuccess: "#07bc0c",
-  colorWarning: "#f1c40f",
-  colorError: "#e74c3c",
-  colorProgressDefault: "linear-gradient(to right, #9d98ff,  #d3d2f8)",
-  mobile: "only screen and (max-width : 480px)",
-  zIndex: 9999,
-  TOP_LEFT: {
-    left: '1em'
-  },
-  TOP_CENTER: {
-    marginLeft: `-${320/2}px`,
-    left: '50%'
-  },
-  TOP_RIGHT: {
-    right: '1em'
-  },
-  BOTTOM_LEFT: {
-    bottom: '2em',
-    left: '1em'
-  },
-  BOTTOM_CENTER: {
-    bottom: '2em',
-    marginLeft: `-${320/2}px`,
-    left: '50%'
-  },
-  BOTTOM_RIGHT: {
-    bottom: '2em',
-    right: '1em'
-  }
-});
-
 export const PLAYER_W = 1;
 export const PLAYER_B = 2;
 var   gameKey;
@@ -311,8 +276,6 @@ class Game extends Component {
         }
     }
 
-    //notify = () => toast("Wow so easy !");
-
     userJoined(joinData) {
         console.log(" user", joinData, "has joined!");
         if(clientPlayer === "1" && joinData === "2") {
@@ -352,5 +315,40 @@ class Game extends Component {
         );
     }
 }
+
+style({
+  width: "320px",
+  colorDefault: "#000",
+  colorInfo: "#3498db",
+  colorSuccess: "#07bc0c",
+  colorWarning: "#f1c40f",
+  colorError: "#e74c3c",
+  colorProgressDefault: "linear-gradient(to right, #9d98ff,  #d3d2f8)",
+  mobile: "only screen and (max-width : 480px)",
+  zIndex: 9999,
+  TOP_LEFT: {
+    left: '1em'
+  },
+  TOP_CENTER: {
+    marginLeft: `-${320/2}px`,
+    left: '50%'
+  },
+  TOP_RIGHT: {
+    right: '1em'
+  },
+  BOTTOM_LEFT: {
+    bottom: '2em',
+    left: '1em'
+  },
+  BOTTOM_CENTER: {
+    bottom: '2em',
+    marginLeft: `-${320/2}px`,
+    left: '50%'
+  },
+  BOTTOM_RIGHT: {
+    bottom: '2em',
+    right: '1em'
+  }
+});
 
 export default Game;
